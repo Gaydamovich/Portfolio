42840cfa8960f6cca60384ccbe2b9ac02165b1d2
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2019-10-20 22:29:14 +0300

    feat: add slider&swiper

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
 //expansion panel
 
-let button = document.querySelector('.btn')
+let buttonPlus = document.querySelector('.btn-plus')
+let buttonMinus = document.querySelector('.btn-minus')
 
-button.onclick = () => {
+buttonPlus.onclick = () => {
+    document.querySelector('.btn-minus').style.display = 'flex'
+    buttonPlus.style.display = 'none'
     let list = document.querySelector('.wrapper-list')
-    list.classList.toggle('hidden')
-    list.classList.toggle('show')
+    list.classList.add('show')
 }
 
-// slider
-
-let images = document.querySelectorAll('.image')
-let current = 0
-
-function slider() {
-    for (let i = 0; i < images.length; i++) {
-        images[i].classList.add('opacity')
-    }
-    images[current].classList.remove('opacity')
-}
-
-document.querySelector('.btn-next').onclick = () => {
-    slider()
-    if ( current + 1 == images.length ) {
-        current = 0
-    } else {
-        current++
-    }
-
-}
-
-document.querySelector('.btn-back').onclick = () => {
-    slider()
-    if ( current - 1 == -1 ) {
-        current = images.length - 1
-    } else {
-        current--
-    }
+buttonMinus.onclick = () => {
+    document.querySelector('.btn-plus').style.display = 'flex'
+    buttonMinus.style.display = 'none'
+    let list = document.querySelector('.wrapper-list')
+    list.classList.remove('show')
 }
 
 // btn-up
@@ -52,3 +30,84 @@ window.addEventListener('scroll', function() {
 document.querySelector('.btn-up').onclick = () => {
     window.scrollTo(0, 0)
 }
+
+
+// slider is ready
+
+let slides = document.querySelectorAll('.content__item')
+let current = 0
+
+function slider() {
+    slides.forEach( slide => slide.classList.add('hidden-slide'))
+    slides[current].classList.remove('hidden-slide')
+}
+
+slider()
+
+function nextSlide() {
+    if (current + 1 == slides.length) {
+        current = 0
+    } else current++
+    slider()
+}
+
+function prevSlide() { 
+    if (current - 1 == -1) {
+        current = slides.length - 1
+    } else current--
+    slider()
+}
+
+document.querySelector('.btn-left').addEventListener('click', prevSlide)
+document.querySelector('.btn-right').addEventListener('click', nextSlide)
+
+
+// swiper
+
+let content = document.querySelector('.content')
+
+let swipeContainer = (el) => {
+
+    let swipe = el
+    let startX = 0
+    let startY = 0
+    let distanceX = 0
+    let distanceY = 0
+
+    let initTime = 0
+    let fullTime = 0
+
+    let allowedTime = 2000
+    let allowDistanceX = 150
+    let allowDistanceY = 50
+
+    swipe.addEventListener('touchstart', (event) => {
+        startX = event.changedTouches[0].pageX
+        startY = event.changedTouches[0].pageY
+        initTime = new Date().getTime()
+
+        event.preventDefault()
+    })
+
+    swipe.addEventListener('touchend', (event) => {
+        distanceX = event.changedTouches[0].pageX - startX
+        distanceY = event.changedTouches[0].pageY - startY
+
+        fullTime = new Date().getTime() - initTime
+
+        if (Math.abs(distanceX) > allowDistanceX && Math.abs(distanceY) < allowDistanceY) {
+            if (fullTime < allowedTime) {
+                if (distanceX < 0) {
+                    nextSlide()
+                } else {
+                    prevSlide()
+                }
+            }
+        }
+        event.preventDefault()
+    })
+
+
+}
+
+swipeContainer(content)
